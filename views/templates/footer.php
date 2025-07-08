2f2f5311f426734b230561bd301c2d6737abdf83
danilofreitas77 <danilocfreitass77@gmail.com>
2025-07-08 08:43:47 -0300

    fazendo o footer e header estáticos

---
+++ views/templates/footer.php
@@ -0,0 +1,37 @@
+<footer class="bg-body-tertiary text-center text-lg-start mt-auto">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Alphaville</h5>
+        <p>
+          Sistema de transparência e controle financeiro do condomínio Alphaville.
+        </p>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Links úteis</h5>
+        <ul class="list-unstyled mb-0">
+          <li><a href="#" class="text-reset">Home</a></li>
+          <li><a href="#" class="text-reset">Controle de Despesas</a></li>
+          <li><a href="#" class="text-reset">Dúvidas</a></li>
+          <li><a href="#" class="text-reset">Contato</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Contato</h5>
+        <p>Email: suporte@alphaville.com</p>
+        <p>Telefone: (81) 99999-9999</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="text-center p-3 bg-secondary text-white">
+    © 2025 Alphaville. Todos os direitos reservados.
+  </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-..." crossorigin="anonymous"></script>
+
+</body>
+</html>
